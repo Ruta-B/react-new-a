5024b9d381cde077b0d0172f569f29573ecd9984
Ruta-B <baumaneruta38@gmail.com>
2025-02-06 20:45:47 +0100

    add axios

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
-import Weather from './Weather';
+import logo from "./logo.svg";
+import "./App.css";
+import Weather from "./Weather";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-     <Weather />
+        <Weather city="Tokyo" />
       </header>
     </div>
   );
--- src/Weather.js
+++ src/Weather.js
@@ -1,7 +1,13 @@
 import React from "react";
+import axios from "axios";
 
-export default function Weather() {
-    return (
-        <h2>Hello</h2>
-    )
+export default function Weather(props) {
+  function handleResponse(response) {
+    alert(`The weather in ${response.data.name} is ${response.data.main.temp} °C`);
+  }
+
+  let apiKey = "ed55b36e362d8733f7d859247cedeaf2";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(handleResponse);
+  return <h2>Hello</h2>;
 }
